cfacef58de89e6475aaeafe902fcaaf8c0cd2e37
serraguti <pacoserranox@gmail.com>
2023-01-17 18:22:42 +0100

    commit

--- AccesoDatosCore/Controllers/PlantillasController.cs
+++ AccesoDatosCore/Controllers/PlantillasController.cs
@@ -13,6 +13,24 @@ namespace AccesoDatosCore.Controllers
             this.repo = repo;
         }
 
+        public IActionResult IncrementarSalarioFunciones()
+        {
+            List<string> funciones = this.repo.GetFunciones();
+            ViewData["FUNCIONES"] = funciones;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult IncrementarSalarioFunciones(string funcion, int incremento)
+        {
+            int modificados = this.repo.IncrementarSalariosFunciones(funcion, incremento);
+            List<string> funciones = this.repo.GetFunciones();
+            ViewData["FUNCIONES"] = funciones;
+            ViewData["MENSAJE"] = "Empleados modificados: " + modificados;
+            List<Plantilla> personas = this.repo.GetPlantillaFuncion(funcion);
+            return View(personas);
+        }
+
         public IActionResult Index()
         {
             List<Plantilla> plantilla = this.repo.GetPlantilla();
